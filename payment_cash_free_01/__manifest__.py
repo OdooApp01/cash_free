6c81e0876aff69eb0512a6ff6003d277e125b943
root <root@DESKTOP-16V093T.localdomain>
2022-08-28 17:24:23 +0530

    [ADD] payment Cash Free

---
+++ payment_cash_free_01/__manifest__.py
@@ -0,0 +1,25 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Payment Acquirer Cash Free 01',
+    'version': '15.0.0',
+    'category': 'Accounting/Payment Acquirers',
+    'sequence': 377,
+    'summary': 'Payment Acquirer: Payment Cash Free Implementation',
+    'description': """
+Cash Free Payment Acquirer for India.
+
+Cash Free payment gateway supports only INR currency.
+""",
+    'depends': ['payment'],
+    'data': [
+        'views/payment_views.xml',
+        'views/payment_payumoney_templates.xml',
+        'data/payment_acquirer_data.xml',
+    ],
+    'application': True,
+    'pre_init_hook':'pre_init_hook',
+    'uninstall_hook': 'uninstall_hook',
+    'license': 'LGPL-3',
+
+}
